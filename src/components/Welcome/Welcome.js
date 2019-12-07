9670ba6572844882885100ff7b28ae39f8f2b772
Michael <mschneider247@gmail.com>
2019-12-07 13:55:30 -0700

    Initial setup and linking for Welcome component

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = (props) => {
+  
+  return (
+    <section>
+      <h2>Welcome to Color_Picker!</h2>
+    </section>
+  )
+}
+
+export default Welcome;
