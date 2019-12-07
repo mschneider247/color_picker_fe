d1681e1e0976459c18467d8549434011bd747c41
Michael <mschneider247@gmail.com>
2019-12-07 13:02:02 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import App from './components/App/App';
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
