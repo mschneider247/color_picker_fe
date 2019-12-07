f46ad420d2a031bca99a23676309636caa5b23fe
Michael <mschneider247@gmail.com>
2019-12-07 13:22:13 -0700

    Allow App to start in the browser and Developer to start test suite

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,20 @@ import './App.css';
 
 export class App extends Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      projects: [],
+      palettes: [],
+    }
   }
 
-  
+  render() {
+    return (
+      <p>This is Color_Picker_FE</p>
+    );
+  }
+
+
 }
+
+export default App;
