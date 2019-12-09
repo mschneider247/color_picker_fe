3346f899942f3ddecc72e53cb48a5fd9f0c0377f
Samuel Coleman <smcoleman19@gmail.com>
2019-12-08 17:38:19 -0700

    Add method for fetching specific palette based on id

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,17 @@
-export const fetchProjects = async () => {
+export const fetchAllProjects = async () => {
 	const response = await fetch('https://color-picker-be.herokuapp.com/api/v1/projects');
 	const data = await response.json();
 	return data;
 };
 
-export const fetchPalettes = async () => {
+export const fetchAllPalettes = async () => {
 	const response = await fetch('https://color-picker-be.herokuapp.com/api/v1/palettes');
+	const data = await response.json();
+	return data;
+};
+
+export const fetchPalette = async (id) => {
+	const response = await fetch(`https://color-picker-be.herokuapp.com/api/v1/palettes/${id}`);
 	const data = await response.json();
 	return data;
 };
