05e00e6f1565ba626e837c992e5f3e8c4637dfec
Samuel Coleman <smcoleman19@gmail.com>
2019-12-08 17:24:58 -0700

    Add method for fetching all palettes

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,4 +2,10 @@ export const fetchProjects = async () => {
 	const response = await fetch('https://color-picker-be.herokuapp.com/api/v1/projects');
 	const data = await response.json();
 	return data;
-}
+};
+
+export const fetchPalettes = async () => {
+	const response = await fetch('https://color-picker-be.herokuapp.com/api/v1/palettes');
+	const data = await response.json();
+	return data;
+};
