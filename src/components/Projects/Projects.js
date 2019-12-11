005572eb5a5f643d532bb85d3350380fc46c2c39
Michael <mschneider247@gmail.com>
2019-12-10 18:04:48 -0700

    Allow user to POST new Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,14 +4,37 @@ class Projects extends Component {
   constructor() {
     super();
     this.state = {
-      currentProject: {}
+      projectName: ''
+    }
+  }
+
+  updateState = (event) => {
+    this.setState({ projectName: event.target.value})
+  }
+
+  addNewProject = () => {
+    if (this.state.projectName !== '') {
+      this.props.addProject(this.state.projectName)
+      this.setState({ projectName: ''})
     }
   }
 
   render () {
     return(
       <section>
-        <p>{this.props.projects}</p>
+        <h3 id="projects">Projects:</h3>
+        <h3>Add A New Project:</h3>
+        <input 
+          className="input-project"
+          type="text"
+          placeholder="Project Name"
+          name="projectName"
+          value={this.state.projectName}
+          onChange={event => this.updateState(event)}
+        />
+        <button onClick={() => this.addNewProject()}>+</button>
+        <br />
+        {this.props.projects}
       </section>
     )
   }
